7f908673f0d6762855bf659b3bc399a6d6944009
Tung Tran Thanh <tung.tran.3295@gmail.com>
2025-02-14 18:42:25 +0700

    Fixed issue

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ if __name__ == '__main__':
     log = logging.getLogger('werkzeug')
     log.disabled = True
     load_config("config/logging.yaml")
-    # app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
